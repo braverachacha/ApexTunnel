31d0be4c4c22cdf93d9279737d23ea0424d6ee1d
braverachacha <braverachacha@gmail.com>
2026-04-10 01:11:58 +0300

    fix: used new domain extention

--- api/src/index.js
+++ api/src/index.js
@@ -1,5 +1,5 @@
 import { createServer } from 'http';
-import { app } from './app.js';
+import app from './app.js';
 import { initWebSocket } from './ws/index.js';
 
 import 'dotenv/config';
@@ -12,5 +12,5 @@ initWebSocket(server);
 
 // app server
 server.listen(PORT, () => {
-  console.log(`Server running on: http://localhost:${PORT}`)
-})
+  console.log(`Server running on: http://localhost:${PORT}`);
+});
